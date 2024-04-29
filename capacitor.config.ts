86704da368d6e67dd252f4a10a7c36188cc19ac1
Jaider Mendoza <jaiderstyvenmendoza@gmail.com>
2024-04-29 13:21:25 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'api-app-ionic',
+  webDir: 'www'
+};
+
+export default config;
